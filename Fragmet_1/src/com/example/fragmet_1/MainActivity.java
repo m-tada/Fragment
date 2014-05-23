5353e4020542ee81812e482da0f5933dbba6b4ee
nssv-05 <nssv-05@SO-PC-116>
2014-05-23 15:49:02 +0900

    初期作成

--- Fragmet_1/src/com/example/fragmet_1/MainActivity.java
+++ Fragmet_1/src/com/example/fragmet_1/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.fragmet_1;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +9,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOoptiomsMenu(){
+		getMenunflater().inflate(R.menu.main, menu);
+		return ture;
 	}
 }
